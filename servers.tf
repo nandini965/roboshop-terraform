006fb6956ad325ebaa75ba3aff273255d50ff220
nandini965 <nandhini965@gmail.com>
2023-05-29 20:09:59 +0530

    vars

--- servers.tf
+++ servers.tf
@@ -6,7 +6,7 @@
 }
 
  data "aws_security_group" "allow all" {
-   name = "allow all"
+   name = "allow-all"
  }
  variable "instance_type" {
    default = "t3.small"
@@ -15,7 +15,8 @@
   resource "aws_instance" "frontend" {
     ami           = data.aws_ami.centos.image_id
     instance_type = var.instance_type
-    vpc_security_group_ids = [ data.aws_security_group.allow all.id ]
+    vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+
   tags = {
     Name = "frontend"
   }
