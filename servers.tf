f33af69f7139ce08245dbdbf475982aac83a89bc
nandini965 <nandhini965@gmail.com>
2023-05-29 19:56:41 +0530

    vars

--- servers.tf
+++ servers.tf
@@ -5,8 +5,8 @@
   name_regex  = "Centos-8-DevOps-Practice"
 }
 
- data "aws_security_group" "allow-all" {
-   name = "allow-all"
+ data "aws_security_group" "allow all" {
+   name = "allow all"
  }
  variable "instance_type" {
    default = "t3.small"
@@ -15,7 +15,7 @@
   resource "aws_instance" "frontend" {
     ami           = data.aws_ami.centos.image_id
     instance_type = var.instance_type
-    vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+    vpc_security_group_ids = [ data.aws_security_group.allow all.id ]
   tags = {
     Name = "frontend"
   }
