f1f1037ebc58c89a9e136f0a9ccd8b037a0a7697
nandini965 <nandhini965@gmail.com>
2023-05-31 20:26:16 +0530

    servers.tf

--- servers.tf
+++ servers.tf
@@ -4,7 +4,6 @@
    most_recent = true
    name_regex  = "Centos-8-DevOps-Practice"
 }
-
  data "aws_security_group" "allow-all" {
    name = "allow-all"
  }
@@ -19,7 +18,7 @@ variable "components" {
    count = length(var.components)
    ami           = data.aws_ami.centos.image_id
    instance_type = var.instance_type
-   vpc_security_group_ids = [ "data.aws_security_group.allow-all.id" ]
+   vpc_security_group_ids = data.aws_security_group.allow-all.id"
 
   tags = {
     Name = "var.components[count.index]"
