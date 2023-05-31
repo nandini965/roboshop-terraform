f0ff5057cc821de9bda166c88253a6a45de2bd7d
nandini965 <nandhini965@gmail.com>
2023-05-31 16:45:40 +0530

    servers.tf

--- servers.tf
+++ servers.tf
@@ -15,7 +15,7 @@
  resource "aws_instance" "frontend" {
    ami           = data.aws_ami.centos.image_id
    instance_type = var.instance_type
-   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
   tags = {
     Name = "frontend"
