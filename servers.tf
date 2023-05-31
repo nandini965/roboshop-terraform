23d20168e37f95bfc264a9ead00a4a85789fcd34
nandini965 <nandhini965@gmail.com>
2023-05-31 16:04:23 +0530

    servers.tf

--- servers.tf
+++ servers.tf
@@ -6,16 +6,15 @@
 }
 
  data "aws_security_group" "allow-all" {
-   name = "allow-all"
+   name =  "allow-all"
  }
   variable "instance_type" {
    default = "t3.small"
  }
-
-  resource "aws_instance" "frontend" {
-    ami           = data.aws_ami.centos.image_id
-    instance_type = var.instance_type
-    vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+ resource "aws_instance" "frontend" {
+   ami           = data.aws_ami.centos.image_id
+   instance_type = var.instance_type
+   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
   tags = {
     Name = "frontend"
