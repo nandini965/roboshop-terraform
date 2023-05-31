2dfa0df48251c91c0e945da7ae957d2dc182859a
nandini965 <nandhini965@gmail.com>
2023-05-31 20:07:20 +0530

    servers.tf

--- servers.tf
+++ servers.tf
@@ -19,7 +19,7 @@ variable "components" {
    count = length(var.components)
    ami           = data.aws_ami.centos.image_id
    instance_type = var.instance_type
-   vpc_security_group_ids = [ "data.aws_security_group.allow-all.id" ]
+   vpc_security_group_ids = [ "data.aws_security_group.allow all.id" ]
 
   tags = {
     Name = "var.components[count.index]"
