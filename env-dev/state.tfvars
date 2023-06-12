8cf239c230bfc660a2864affddb6074cdcdc73cc
nandini965 <nandhini965@gmail.com>
2023-06-12 19:36:55 +0530

    state

---
+++ env-dev/state.tfvars
@@ -0,0 +1,4 @@
+
+    bucket = "terraform92"
+    key    = "roboshop/dev/terraform.tfstate"
+    region = "us-east-1"
--- state.tf
+++ state.tf
@@ -1,8 +1,4 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform92"
-    key    = "roboshop/dev/terraform.tfstate"
-    region = "us-east-1"
-  }
+ terraform {
+  backend "s3" {}
 }
 
