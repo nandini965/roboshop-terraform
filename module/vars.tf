c0a4eb6e002d7188e4cc75351454f67fcbb1db03
nandini965 <nandhini965@gmail.com>
2023-06-24 00:33:20 +0530

    main.yml

--- module/vars.tf
+++ module/vars.tf
@@ -2,4 +2,7 @@ variable "env" {}
 variable "instance_type" {}
 variable "component_name" {}
 variable "password" {}
+variable "provisioner" {
+  default = false
+}
 
--- vars.tf
+++ vars.tf
@@ -1,6 +1,3 @@
 variable "env" {}
 variable "app_servers" {}
 variable "database_servers" {}
-variable "provisioner" {
-default = false
-}
