e36de551f5e880d33c3859bb638d0de600a8199d
nandini965 <nandhini965@gmail.com>
2023-06-24 00:26:02 +0530

    main.yml

--- servers.tf
+++ servers.tf
@@ -5,7 +5,6 @@ module "database-servers" {
   env = var.env
   instance_type = each.value["instance_type"]
   password = lookup(each.value, "password", "null")
-  provisioner = true
 }
 
 module "app-servers" {
