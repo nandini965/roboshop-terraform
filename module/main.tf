4ecd11c5390e030eec1439ddb111e651fb0ef2cc
nandini965 <nandhini965@gmail.com>
2023-07-07 10:56:35 +0530

    shipping

--- module/main.tf
+++ module/main.tf
@@ -57,27 +57,25 @@ resource "aws_iam_role_policy" "ssm-ps-policy" {
   role = aws_iam_role.role.id
 
   policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
   {
-    "Version": "2012-10-17",
-    "Statement": [
-  {
-    "Sid": "VisualEditor0",
-    "Effect": "Allow",
-    "Action": [
+    "Sid" : "VisualEditor0",
+    "Effect" : "Allow",
+    "Action" : [
     "ssm:GetParameterHistory",
     "ssm:GetParametersByPath",
     "ssm:GetParameters",
     "ssm:GetParameter"
   ],
-    "Resource": "arn:aws:ssm:us-east-1:622771222299:parameter/${var.env}-${var.component_name}.*"
+    "Resource" : "arn:aws:ssm:us-east-1:622771222299:parameter/${var.env}-${var.component_name}.*"
   },
   {
-    "Sid": "VisualEditor1",
-    "Effect": "Allow",
-    "Action": "ssm:DescribeParameters",
-    "Resource": "*"
-
-      }
-    ]
+    "Sid" : "VisualEditor1",
+    "Effect" : "Allow",
+    "Action" : "ssm:DescribeParameters",
+    "Resource" : "*"
+  }
+  ]
   })
- }
+  }
