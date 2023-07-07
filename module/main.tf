8ef48a1ed47a3cafd750791657c6eaa6d22b412e
nandini965 <nandhini965@gmail.com>
2023-07-07 10:13:44 +0530

    shipping

--- module/main.tf
+++ module/main.tf
@@ -51,3 +51,33 @@ resource "aws_iam_role" "role" {
     tag-key = "${var.component_name}-${var.env}-role"
   }
 }
+
+resource "aws_iam_role_policy" "test_policy" {
+  name = "test_policy"
+  role = aws_iam_role.test_role.id
+
+  policy = jsonencode({
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+  {
+    "Sid": "VisualEditor0",
+    "Effect": "Allow",
+    "Action": [
+    "ssm:GetParameterHistory",
+    "ssm:GetParametersByPath",
+    "ssm:GetParameters",
+    "ssm:GetParameter"
+  ],
+    "Resource": "arn:aws:ssm:us-east-1:622771222299:parameter/${var.env}-${var.component_name}"
+  },
+  {
+    "Sid": "VisualEditor1",
+    "Effect": "Allow",
+    "Action": "ssm:DescribeParameters",
+    "Resource": "*"
+
+      },
+    ]
+  })
+}
