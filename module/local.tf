b7101078dcd34182d2d26b74b81daba35644921e
nandini965 <nandhini965@gmail.com>
2023-07-31 19:47:24 +0530

    filebeat

--- module/local.tf
+++ module/local.tf
@@ -3,11 +3,17 @@ locals {
   db_commands = [
   "rm -rf roboshop-shell",
   "git clone https://github.com/nandini965/roboshop-shell.git",
-"cd roboshop-shell",
-"sudo bash ${var.component_name}.sh ${var.password}"
+  "cd roboshop-shell",
+  "sudo bash ${var.component_name}.sh ${var.password}"
 ]
   app_commands = [
 "sudo labauto ansible",
  "ansible-pull -i localhost -U https://github.com/nandini965/roboshop-ansible roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
 ]
+  db_tags {
+    Name = "${var.component_name}-${var.env}"
+  }
+  app_tags {
+    Name = "${var.component_name}-${var.env}"
+  }
 }
