7492d69f1f360c49080f8e01fec00a709c3dae2e
nandini965 <nandhini965@gmail.com>
2023-07-31 21:39:14 +0530

    filebeat

--- module/local.tf
+++ module/local.tf
@@ -1,19 +1,20 @@
 locals {
-  name = var.env != "" ? "${var.component_name}-${var.env}" : var.component_name
+  name        = var.env != "" ? "${var.component_name}-${var.env}" : var.component_name
   db_commands = [
-  "rm -rf roboshop-shell",
-  "git clone https://github.com/nandini965/roboshop-shell.git",
-  "cd roboshop-shell",
-  "sudo bash ${var.component_name}.sh ${var.password}"
-]
+    "rm -rf roboshop-shell",
+    "git clone https://github.com/nandini965/roboshop-shell.git",
+    "cd roboshop-shell",
+    "sudo bash ${var.component_name}.sh ${var.password}"
+  ]
   app_commands = [
-"sudo labauto ansible",
- "ansible-pull -i localhost -U https://github.com/nandini965/roboshop-ansible roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
-]
+    "sudo labauto ansible",
+    "ansible-pull -i localhost -U https://github.com/nandini965/roboshop-ansible roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
+  ]
   db_tags = {
-    Name = ${var.component_name}-${var.env}
+    Name = "${var.component_name}-${var.env}"
   }
   app_tags = {
-    Name = ${var.component_name}-${var.env}
+    Name = "${var.component_name}-${var.env}"
     Monitor = "true"
   }
+}
