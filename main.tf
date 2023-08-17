e2e4941277f76a7d2ba92a4c0b3e8d5212353401
nandini965 <nandhini965@gmail.com>
2023-08-17 22:13:46 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 module "vpc" {
   source = "git::https://github.com/nandini965/tf-vpc-module.git"
+  env = var.env
 
   for_each = var.vpc
   cidr_block = each.value["cidr_block"]
-  env = var.env
+
 }
