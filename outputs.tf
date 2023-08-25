b9612b8f03a49fe7ea64516c64dd376ef1e42988
nandini965 <nandhini965@gmail.com>
2023-08-26 00:34:43 +0530

    filebeat

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "vpc" {
-  value = element(lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null), 0)
+  value = lookup(lookup(module.vpc, "main", null), "subnets", null)
 }
 
 //lookup(lookup(module.vpc, "main", null), "subnets", null)
