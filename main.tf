93f74b14c41f3ea1acd0f613907aad405c447d07
nandini965 <nandhini965@gmail.com>
2023-08-26 00:59:45 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -12,7 +12,6 @@ module "web" {
  source        = "git::https://github.com/nandini965/tf-module-app.git"
   for_each      = var.app
   instance_type = each.value["instance_type"]
-  subnet_name = each.value["subnet_name"]
   subnet_id     = element(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "subnet_ids", null), 0)
 }
 #lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
