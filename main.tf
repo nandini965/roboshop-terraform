72593af9c2c0a34a89dc8cae36fdf960562bc1f5
nandini965 <nandhini965@gmail.com>
2023-08-28 19:28:17 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -8,12 +8,12 @@ module "vpc" {
   vpc_id = var.vpc
  azs = "var.azs"
 }
-//module "app" {
- //source        = "git::https://github.com/nandini965/tf-module-app.git"
-  //for_each      = var.app
-  //instance_type = each.value["instance_type"]
-  //subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
-//}
+  module "app" {
+  source        = "git::https://github.com/nandini965/tf-module-app.git"
+  for_each      = var.app
+  instance_type = each.value["instance_type"]
+  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+}
 #lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
 
 
