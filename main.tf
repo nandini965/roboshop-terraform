7d7cd8ab7e259c03a4337314084648cfb70cc1da
nandini965 <nandhini965@gmail.com>
2023-09-05 16:03:54 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -6,7 +6,6 @@ module "vpc" {
  tags        = local.tags
  env = var.env
   vpc_id = var.vpc
- azs = var.azs
 }
   module "app" {
     source           = "git::https://github.com/nandini965/tf-module-app.git"
