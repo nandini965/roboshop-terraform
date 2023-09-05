524d159112c5a22ab10ce99080b54b90966ccbda
nandini965 <nandhini965@gmail.com>
2023-09-05 16:05:48 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-module "vpc" {
-  source     = "git::https://github.com/nandini965/tf-vpc-module.git"
-  for_each   = var.vpc
-  cidr_block = each.value["cidr_block"]
-  subnets    = each.value["subnets"]
- tags        = local.tags
- env = var.env
-  vpc_id = var.vpc
-}
+//module "vpc" {
+  //source     = "git::https://github.com/nandini965/tf-vpc-module.git"
+  //for_each   = var.vpc
+  //cidr_block = each.value["cidr_block"]
+  //subnets    = each.value["subnets"]
+ //tags        = local.tags
+ //env = var.env
+  //vpc_id = var.vpc
+//}//
   module "app" {
     source           = "git::https://github.com/nandini965/tf-module-app.git"
     for_each         = var.app
