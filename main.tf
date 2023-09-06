5f9060fef90cd0a08167dea65e82eea4a0fd26e7
nandini965 <nandhini965@gmail.com>
2023-09-06 20:23:22 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -1,29 +1,29 @@
-//module "vpc" {
-  //source     = "git::https://github.com/nandini965/tf-vpc-module.git"
-  //for_each   = var.vpc
-  //cidr_block = each.value["cidr_block"]
-  //subnets    = each.value["subnets"]
- //tags        = local.tags
- //env = var.env
-//  vpc_id = var.vpc
- // default_vpc_id = var.default_vpc_id
-  //default_vpc_id = var.default_vpc_cidr
-//}
-  //module "app" {
-    //source           = "git::https://github.com/nandini965/tf-module-app.git"
-    //for_each         = var.app
-    //instance_type    = each.value["instance_type"]
-    //name             = each.value["name"]
-    //desired_capacity = each.value["desired_capacity"]
-    //min_size         = each.value["min_size"]
-    //max_size         = each.value["max_size"]
-    //env              = var.env
-    //bastion_cidr     = var.bastion_cidr
-    //tags = local.tags
-    //subnet_ids       = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnets_ids", null)
-    //vpc_id          = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-    //allow_app_cidr   = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnets_cidrs", null)
- //}
+module "vpc" {
+  source     = "git::https://github.com/nandini965/tf-vpc-module.git"
+  for_each   = var.vpc
+  cidr_block = each.value["cidr_block"]
+  subnets    = each.value["subnets"]
+ tags        = local.tags
+ env = var.env
+ vpc_id = var.vpc
+  default_vpc_id = var.default_vpc_id
+  default_vpc_id = var.default_vpc_cidr
+}
+  module "app" {
+    source           = "git::https://github.com/nandini965/tf-module-app.git"
+    for_each         = var.app
+    instance_type    = each.value["instance_type"]
+    name             = each.value["name"]
+    desired_capacity = each.value["desired_capacity"]
+    min_size         = each.value["min_size"]
+    max_size         = each.value["max_size"]
+    env              = var.env
+    bastion_cidr     = var.bastion_cidr
+    tags = local.tags
+    subnet_ids       = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnets_ids", null)
+    vpc_id          = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+    allow_app_cidr   = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnets_cidrs", null)
+ }
 
 
 
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-//output "vpc" {
-  //value = lookup(lookup(module.vpc, "main", null), "subnets", null)
-//}
+output "vpc" {
+  value = lookup(lookup(module.vpc, "main", null), "subnets", null)
+}
 
