ea241d0ac21d9b436335406c5a34b72f1ad74031
nandini965 <nandhini965@gmail.com>
2023-09-14 01:22:07 +0530

    filebeat

---
+++ locals.tf
@@ -0,0 +1,10 @@
+locals {
+  vpc_id = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  tags = {
+    business_unit = "ecommerce"
+    business_type = "retail"
+    project_name  = "RoboShop"
+    cost_center   = 100
+    env           = var.env
+  }
+}
