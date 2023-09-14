33b2298c475eeef788e99e9018eb7e7a485a3294
nandini965 <nandhini965@gmail.com>
2023-09-14 18:17:43 +0530

    filebeat

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -69,14 +69,14 @@ kms_arn = "arn:aws:kms:us-east-1:121558927746:key/e4d7c861-f016-44b5-8edd-3e79df
      //subnet_name = "app"
    //}
 //}
-//docdb = {
-  //main = {
-    //subnet_name = "db"
-    //allow_db_cidr = "app"
-    //engine_version = "4.0.0"
-   // instance_count = 1
-   // instance_class = "db.t3.medium"
- //}
-//}
+docdb = {
+  main = {
+    subnet_name = "db"
+    allow_db_cidr = "app"
+    engine_version = "4.0.0"
+    instance_count = 1
+    instance_class = "db.t3.medium"
+ }
+}
 
 
--- main.tf
+++ main.tf
@@ -10,11 +10,11 @@ module "vpc" {
   default_vpc_cidr = var.default_vpc_cidr
   default_vpc_rtid = var.default_vpc_rtid
 }
- // module "app" {
+ //module "app" {
    // source           = "git::https://github.com/nandini965/tf-module-app.git"
-   // for_each         = var.app
+    //for_each         = var.app
     //instance_type    = each.value["instance_type"]
-   // name             = each.value["name"]
+   //name             = each.value["name"]
     //desired_capacity = each.value["desired_capacity"]
     //min_size         = each.value["min_size"]
     //max_size         = each.value["max_size"]
@@ -24,23 +24,23 @@ module "vpc" {
     //subnet_ids       = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnets_ids", null)
     //vpc_id          = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
     //allow_app_cidr   = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnets_cidrs", null)
-// }
+ //}
 
 
 
-//module "docdb" {
-  //source = "git::https://github.com/nandini965/tf-module-docdb.git"
+module "docdb" {
+  source = "git::https://github.com/nandini965/tf-module-docdb.git"
 
-  //for_each       = var.docdb
-  //subnets        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
-  //allow_db_cidr  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
-  //engine_version = each.value["engine_version"]
-//  instance_count = each.value["instance_count"]
-  //instance_class = each.value["instance_class"]
+  for_each       = var.docdb
+  subnets        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+  allow_db_cidr  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
+  engine_version = each.value["engine_version"]
+  instance_count = each.value["instance_count"]
+  instance_class = each.value["instance_class"]
 
 
-  //tags    = local.tags
- // env     = var.env
-  //vpc_id  = local.vpc_id
-  //kms_arn = var.kms_arn
-//}
+  tags    = local.tags
+  env     = var.env
+  vpc_id  = local.vpc_id
+  kms_arn = var.kms_arn
+}
