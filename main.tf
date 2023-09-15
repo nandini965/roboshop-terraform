c7f39c81f43c7c3efe74a7d6dbc187ad36cd4170
nandini965 <nandhini965@gmail.com>
2023-09-15 20:08:04 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -45,21 +45,21 @@ module "docdb" {
   kms_arn = var.kms_arn
 }
 
-//module "rds" {
- // source = "git::https://github.com/nandini965/tf-module-rds.git"
+module "rds" {
+  source = "git::https://github.com/nandini965/tf-module-rds.git"
 
-  //for_each       = var.rds
-  //subnets        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnets_ids", null)
- // allow_db_cidr  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
-  //engine_version = each.value["engine_version"]
-  //instance_count = each.value["instance_count"]
-  //instance_class = each.value["instance_class"]
+  for_each       = var.docdb
+  subnets        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnets_ids", null)
+  allow_db_cidr  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
+  engine_version = each.value["engine_version"]
+  instance_count = each.value["instance_count"]
+  instance_class = each.value["instance_class"]
 
 
- // tags    = local.tags
-  //env     = var.env
-  //vpc_id  = local.vpc_id
-  //kms_arn = var.kms_arn
-//}
+ tags    = local.tags
+  env     = var.env
+  vpc_id  = local.vpc_id
+  kms_arn = var.kms_arn
+}
 
 
