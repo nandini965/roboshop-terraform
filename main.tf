c1a12e10fc2921e68cfca3b1419fe41415adfcd5
root <root@ip-172-31-94-29.ec2.internal>
2023-09-28 15:50:15 +0000

    Changes Made by Raghu

--- main.tf
+++ main.tf
@@ -95,16 +95,16 @@ module "rabbitmq" {
   bastion_cidr = var.bastion_cidr
 }
 
-#module "alb" {
-# source = "git::https://github.com/nandini965/tf-module-alb.git"
+module "alb" {
+ source = "git::https://github.com/nandini965/tf-module-alb.git"
 
-#  for_each       = var.alb
-#  subnets        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
-#  allow_alb_cidr = each.value["name"] == "public" ? ["0.0.0.0/0"] : lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_alb_cidr"], null), "subnet_cidrs", null)
-#  name           = each.value["name"]
-#  internal       = each.value["internal"]
+  for_each       = var.alb
+  subnets        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnets_ids", null)
+  allow_alb_cidr = each.value["name"] == "public" ? ["0.0.0.0/0"] : lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_alb_cidr"], null), "subnet_cidrs", null)
+  name           = each.value["name"]
+  internal       = each.value["internal"]
 
-#  tags   = local.tags
-#  env    = var.env
-#  vpc_id = local.vpc_id
-#}
+  tags   = local.tags
+  env    = var.env
+  vpc_id = local.vpc_id
+}
