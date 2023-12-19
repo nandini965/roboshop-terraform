9dcd45c89f6081bea96e43c3dbae715c9fcaaef3
root <root@ip-172-31-47-250.ec2.internal>
2023-12-19 15:41:33 +0000

    Commits from Raghu

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ module "docdb" {
 
  for_each       = var.docdb
   subnets        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnets_ids", null)
-  allow_db_cidr  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
+  allow_db_cidr  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnets_cidr", null)
   engine_version = each.value["engine_version"]
   instance_count = each.value["instance_count"]
    instance_class = each.value["instance_class"]
