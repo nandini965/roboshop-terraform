bcec87e5823cca753e333d30c8beea118ae1f13d
nandini965 <nandhini965@gmail.com>
2023-12-22 16:27:45 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ module "docdb" {
  source = "git::https://github.com/nandini965/tf-module-docdb.git"
 
  for_each       = var.docdb
-  subnets        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnets_id", null)
+  subnets        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnets_ids", null)
   allow_db_cidr  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnets_cidr", null)
   engine_version = each.value["engine_version"]
   instance_count = each.value["instance_count"]
