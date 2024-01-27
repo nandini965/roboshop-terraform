e022aead0b6ca77f38eacd3b8ff903b9762626d6
nandini965 <nandhini965@gmail.com>
2024-01-27 16:16:11 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -126,14 +126,14 @@ module "vpc" {
 
 module "eks" {
   source             = "git::https://github.com/nandini965/tf-module-eks.git"
-#  ENV                = var.env
-#  eks_version        = 1.27
-#  PRIVATE_SUBNET_IDS = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnet_ids", null)
-#  PUBLIC_SUBNET_IDS  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "public", null), "subnet_ids", null)
-#  DESIRED_SIZE       = 2
-#  MAX_SIZE           = 2
-#  MIN_SIZE           = 2
-#  kms_arn            = var.kms_arn
+  ENV                = var.env
+  eks_version        = 1.27
+  PRIVATE_SUBNET_IDS = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnet_ids", null)
+  PUBLIC_SUBNET_IDS  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "public", null), "subnet_ids", null)
+  DESIRED_SIZE       = 2
+  MAX_SIZE           = 2
+  MIN_SIZE           = 2
+  kms_arn            = var.kms_arn
 }
 
 #### Load Runner
