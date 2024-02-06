81b447f3a65ec60eb13f8c2eb75ea7998a7f0efb
nandini965 <nandhini965@gmail.com>
2024-02-07 00:29:11 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -126,7 +126,7 @@ module "alb" {
 
 module "eks" {
   source             = "git::https://github.com/nandini965/tf-module-eks.git"
-  ENV                = var.env
+   ENV                = var.env
   eks_version        = 1.27
   PRIVATE_SUBNET_IDS = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnet_ids", null)
   PUBLIC_SUBNET_IDS  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "public", null), "subnet_ids", null)
