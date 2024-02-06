885d1ab881f82ceafafbf05d9564b2b18d07b3c4
nandini965 <nandhini965@gmail.com>
2024-02-07 00:22:20 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -124,17 +124,17 @@ module "alb" {
   domain_id    = var.domain_id
 }
 
-#module "eks" {
-#  source             = "git::https://github.com/nandini965/tf-module-eks.git"
-#  ENV                = var.env
-#  eks_version        = 1.27
-#  PRIVATE_SUBNET_IDS = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnet_ids", null)
-#  PUBLIC_SUBNET_IDS  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "public", null), "subnet_ids", null)
-#  DESIRED_SIZE       = 2
-#  MAX_SIZE           = 2
-#  MIN_SIZE           = 2
-#  kms_arn            = var.kms_arn
-#}
+module "eks" {
+  source             = "git::https://github.com/nandini965/tf-module-eks.git"
+  ENV                = var.env
+  eks_version        = 1.27
+  PRIVATE_SUBNET_IDS = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnet_ids", null)
+  PUBLIC_SUBNET_IDS  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "public", null), "subnet_ids", null)
+  DESIRED_SIZE       = 2
+  MAX_SIZE           = 2
+  MIN_SIZE           = 2
+  kms_arn            = var.kms_arn
+}
 
 #### Load Runner
 data "aws_ami" "ami" {
