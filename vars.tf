07423b466b498679943f1b94312eeba0ca22ff04
nandini965 <nandhini965@gmail.com>
2024-02-27 17:46:51 +0530

    filebeat

--- vars.tf
+++ vars.tf
@@ -1,18 +1,18 @@
 variable "env" {}
 variable "vpc" {}
-#variable "app" {}
+variable "app" {}
 variable "bastion_cidr" {}
 variable "default_vpc_id" {}
 variable "default_vpc_cidr" {}
 variable "default_vpc_rtid" {}
 variable "kms_arn" {}
-#variable "elasticache" {}
-#variable "rabbitmq" {}
-#variable "alb" {}
+variable "elasticache" {}
+variable "rabbitmq" {}
+variable "alb" {}
 variable "domain_name" {}
 variable "domain_id" {}
 variable "monitor_cidr" {}
-#variable "rds" {}
-#variable "docdb" {}
+variable "rds" {}
+variable "docdb" {}
 
 
